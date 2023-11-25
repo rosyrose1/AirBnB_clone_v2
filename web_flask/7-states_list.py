b8dd52330663b9ada43a1528fe1da9edcff84f7f
rosyrose1 <rosinaantwi96@gmail.com>
2023-11-25 05:06:35 -0800

    States lists

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,6 @@
 """Start a Flask web application"""
 
 from models import storage
-from models.city import City
 from models.state import State
 from flask import Flask, render_template
 
@@ -11,21 +10,19 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/states_list')
+def states_list():
+    """Shows an HTML page that lists all states"""
+    states = storage.all(State).values()
+    newStates = sorted(states, key=lambda s: s.name)
+    return render_template('7-states_list.html', states=newStates)
+
+
 @app.teardown_appcontext
-def teardown(self):
-    """Terminate the existing session in SQLAlchemy"""
+def db_teardown(self):
+    """Terminates the current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/cities_by_states')
-def cities_by_state():
-    """Show an HTML page containing a list of all State objects in DBStorage,
-    with the states sorted alphabetically by name."""
-    states = storage.all(State).values()
-    newStates = sorted(states, key=lambda s: s.name)
-    return render_template('8-cities_by_states.html', states=newStates)
-
-
 if __name__ == "__main__":
-    storage.reload()
     app.run(host='0.0.0.0', port=5000)
